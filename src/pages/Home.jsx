3141cfd5236435640ef5da80aec736ee8c6ca084
KamilaSultanbekova <kaamiillkss@gmail.com>
2025-07-21 21:11:33 +0500

    all components are updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import HomeC from '../components/HomeC'
-import Aboutus from '../components/Aboutus'
-import Events from '../components/Events'
-import Menu from '../components/Menu'
+import React from "react";
+import Events from "../assets/eventsPhoto.png";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import HomeC from "../components/HomeC";
+import Aboutus from "../components/Aboutus";
+import Menu from "../components/Menu";
 
 export default function Home() {
   return (
-   <div className='container mx-auto'>
-    <Navbar/>
-    <HomeC/>
-    <Menu/>
-    <Events/>
-    <Aboutus/>
-   
-    <Footer/>
-   </div>
+    <div className="container mx-auto">
+      <Navbar />
+      <HomeC />
+      <Menu />
+      <div className="bg-[#1a0c06]">
+        <img src={Events} className="bg-[#1a0c06] w-350 mx-auto" />
+      </div>
+      <Aboutus />
 
-  )
+      <Footer />
+    </div>
+  );
 }
